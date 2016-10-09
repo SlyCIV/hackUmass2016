c6cb7e42859739e015969d44b00be5b660276e5f
Kirs <kirs.l.imsong@gmail.com>
2016-10-09 06:16:04 -0400

    Final

--- sketch_oct08a/sketch_oct08a.ino
+++ sketch_oct08a/sketch_oct08a.ino
@@ -4,15 +4,15 @@ SevSeg sevseg;
 
 const int analogPin = 1;
 const int tempPin = 3;
-const int threshHold = 300; //Sound threshold
-const int increments = 30; //Size of array and stuff
+const int threshHold = 700; //Sound threshold
+const int knockThreshHold = 700; //Knock threshold
+const int increments = 50; //Size of array and stuff
 
-int soundLevel = 0;
+int soundLevel = 0; //The sound level read from the mic
 int count = 0;
-int successcount = 30;
-int total = 0;
 int a[increments];
-boolean once;
+int knock = 0;
+int strikes = 0;
 
 void setup() {
  byte numDigits = 4; 
@@ -25,54 +25,56 @@ void setup() {
   Serial.begin(9600);
   //pinMode(tempPin, INPUT);
 
+  //fills array A with zeros
   fillArrayA();
 }
 
 void loop() {
   soundLevel = analogRead(analogPin);
+  if(soundLevel>900 || soundLevel<400){
+    soundLevel = 500;
+  }
+  a[count] = soundLevel;
   
   Serial.print("Sound: ");
   Serial.println(soundLevel);
+  //Serial.print("Sound Average: ");
+  //Serial.println(averageOfA());
   //Serial.print("knocker: ");
   //Serial.println(digitalRead(tempPin));
 
-  sevseg.setNumber(flip(soundLevel) ,0);
-
-  a[count] = thresh(soundLevel);
-  count++;
-
-  total = sumOfA();
-  Serial.print("Total: ");
-  Serial.println(total);
-
-  if(count == 50){
-    count = 0;
+  if(soundLevel > 800){
+    strikes++;
   }
-
-  //Sounds our text if we're loud
-  if(total == 10){
+  //Sends out text if we're loud
+  if((averageOfA() > threshHold) || (strikes > 10)){
+    strikes = 0;
     Serial.println("Pattern: loud");
-    delay(10000);
+    delay(1000);
   }
 
-  //Knock
-  if(total <= 7 && total >= 4){
-    successcount--;
-    if(successcount == 0){
-      Serial.println("Pattern: knock");
-      delay(10000);
-      successcount = 10;
-    }
+  //Sends out text if knock
+  knock = knockAverageOfA();
+  //Serial.print("Knock Average: ");
+  //Serial.println(knock);
+  if((knock > knockThreshHold) && (shortAverageOfA() < knockThreshHold-30)){
+    Serial.println("Pattern: knock");
+    delay(1000);
   }
 
-  if(total == 0){
-    successcount = 10;
-  }
-
-  //Maintains a value 
+  //Sends output to display
+  sevseg.setNumber(flip(soundLevel),0);
+  
+  //Maintains a value on the screen and keeps a delay of 10ms 
   for(int i = 0; i<10; i++){
   delay(1);
   sevseg.refreshDisplay();
+  }
+
+  //increments the count variable
+  count++;
+  if(count == 50){
+    count = 0;
   }
 }
 
@@ -98,7 +100,35 @@ void fillArrayA(){
   }
 }
 
+int averageOfA(){
+  int sum = 0;
+  for(int i =0; i < 50; i++){
+  sum = sum + a[i];
+  }
+  return (sum/50);
+}
+
+int knockAverageOfA(){
+  int sum = 0;
+  for(int i =10; i < 20; i++){
+  sum = sum + a[i];
+  }
+  return (sum/10);
+}
+
+int shortAverageOfA(){
+  int sum = 0;
+  for(int i=0; i<10; i++){
+    sum = sum +a[i];
+  }
+  for(int i=20; i<50; i++){
+    sum = sum +a[i];
+  }
+  return (sum/45);
+}
+
 //Rturns either 0 or 1 
+/*
 int thresh(int input){
   if(input > threshHold){
     return 1;
@@ -106,8 +136,10 @@ int thresh(int input){
     return 0;
     }
 }
+*/
 
 //Adds up (counts) the values in the a array of 50 ints (either 0 or 1) and returns an int
+/*
 int sumOfA(){
   int count = 0;
   for(int i = 0; i <increments; i++){
@@ -115,3 +147,5 @@ int sumOfA(){
   }
   return count;
 }
+*/
+
